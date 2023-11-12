0d9448eb3a12ef8de5e8b0696cef7f3e5d62aaa7
Enzo De Sena <3883487+enzodesena@users.noreply.github.com>
2023-11-12 18:08:06 +0000

    Added initial freq domain implementation (not working)

---
+++ SimulationFrequency.m
@@ -0,0 +1,151 @@
+%SIMULATION  Class implementing the Scattering Delay Network (SDN) algorithm
+%
+%   See also Position, Room, Shape
+%
+%   Author: Enzo De Sena
+%  
+%   Copyright (c) 2010, Enzo De Sena, UK
+%   All rights reserved.
+%   
+%   Please notice that this algorithm is protected by USPTO patent:
+%   E. De Sena, H. Hac?habibo?lu, and Z. Cvetkovi?, inventors; 
+%   King's College London, assignee, "Electronic Device with Digital 
+%   Reverberator and Method", US Patent n. 8,908,875, filed 2/2/2012, 
+%   granted 09/12/2014.
+%   For any queries, please contact Enzo De Sena at enzodesena AT gmail DOT com
+classdef SimulationFrequency < handle
+    properties
+        room
+        source
+        microphone
+        frameLength = 1;
+        NSamples
+    end
+
+
+    properties (Access='private')
+        junctions
+        sPropLines
+        mPropLines
+        smPropLine
+    end
+    
+    methods
+        function output = run(this, verbose)
+            if nargin < 2
+                verbose = true;
+            end
+            
+            %%% Initialize variables
+            
+            M = 6;  % 3D case
+            
+            this.initialise(M);
+            
+            
+        end
+    end
+    
+    methods(Access='private')
+        function initialise(this, M)
+            FS = this.source.signal.FS;
+
+            % junctions is the vector containing all the junction
+            % objects.
+            this.junctions = cell(1,M);
+            for i=1:M
+                junction = Junction();
+                
+                %%% Run the ray-tracing module
+                junction.position = getReflectPos(this.room, i, this.source.position, this.microphone.position);
+                this.junctions{i} = junction;
+            end
+            
+            for i=1:M
+                for j=1:M
+                    if i==j
+                        continue;
+                    end
+                    
+                    % The offset -1 means that we want a propagation line
+                    % with delaysample - 1. This is due to the way the
+                    % updating at the junction is made, which intrinsecally
+                    % delays the output by one sample.
+                    propLine = PropLine(this.junctions{i}, this.junctions{j}, FS, -1);
+                    
+                    % Initialize the propagation lines with empty frames
+                    propLine.setNextFrame(zeros(1,this.frameLength));
+                    
+                    % Tell the in and out junctions that this is their
+                    % propagation line.
+                    this.junctions{i}.addPropLineOut(propLine);
+                    this.junctions{j}.addPropLineIn(propLine);
+
+                    propLine.attenuation = 1;
+                end
+            end
+            
+            % TBD: define the filters
+            for i=1:M
+                this.junctions{i}.wallAttenuation = this.room.wallAttenuations{i}; %this.room.wallAttenuations{i};
+                this.junctions{i}.setWallFilter(this.room.wallFilters{i});
+            end
+            
+            
+            
+            % Define source propagation line
+            this.sPropLines = cell(1,M);
+            sDummyJunction = Junction();
+            sDummyJunction.position = this.source.position;
+            firstSourceFrame = this.source.signal.getFrame(1, this.frameLength).data;
+            for i=1:M
+                propLine = PropLine(sDummyJunction, this.junctions{i}, FS);
+                propLine.setNextFrame(firstSourceFrame);
+                this.sPropLines{i} = propLine;
+            end
+            
+            % Define microphone propagation line
+            this.mPropLines = cell(1,M);
+            mDummyJunction = Junction();
+            mDummyJunction.position = this.microphone.position;
+            for i=1:M
+                mPropLine = PropLine(this.junctions{i}, mDummyJunction, FS);
+                
+                distSourceJunct = Position.distance(this.junctions{i}.position, sDummyJunction.position);
+                distJunctMic = Position.distance(this.junctions{i}.position, mDummyJunction.position);
+                
+                
+                % We use this attenuation in such a way that the first
+                % order reflections have an exact attenuation. (We don't
+                % have (mPropLine.c ./ FS) at the numerator, because it is
+                % already at the numerator of the attenuation of the
+                % propLine between source and junction.
+                mPropLine.attenuation = 1 / (1 + distJunctMic/distSourceJunct);
+                this.mPropLines{i} = mPropLine;
+            end
+            
+            % Define one last propagation line between source and
+            % microphone. This line models the LOS component.
+            this.smPropLine = PropLine(sDummyJunction, mDummyJunction, FS);
+            this.smPropLine.setNextFrame(firstSourceFrame);
+
+    
+            for i=1:M
+                Gamma_S(i) = 1;
+                Delta_S(i) = z.^(-this.sPropLines{i}.delaySamples);
+                G_S(i) = this.sPropLines{i}.attenuation;
+                Gamma_M(i) = 1;
+                Delta_M(i) = z.^(-this.mPropLines{i}.delaySamples);
+                G_M(i) = this.mPropLines{i}.attenuation;
+            end
+
+            
+            UCell = repmat({ones(M-1,1)}, 1, M);
+            U = blkdiag(UCell{:});
+            
+            Hbar = 
+        end
+    end
+end
+
+
